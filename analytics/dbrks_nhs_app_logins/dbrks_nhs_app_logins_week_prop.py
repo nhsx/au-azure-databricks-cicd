b9bf9595910b4b929745a870f4747390f0db1b77
mattia.ficarelli@nhsx.nhs.uk <mattia.ficarelli@nhsx.nhs.uk>
2022-08-23 09:59:33 +0000

    Refactor of NHS app login notebooks for migration
    Refactor of NHS app login notebooks for migration

--- analytics/dbrks_nhs_app_logins/dbrks_nhs_app_logins_week_prop.py
+++ analytics/dbrks_nhs_app_logins/dbrks_nhs_app_logins_week_prop.py
@@ -15,8 +15,8 @@ USAGE:
                 ...
 CONTRIBUTORS:   Everistus Oputa, Mattia Ficarelli, Chris Todd
 CONTACT:        data@nhsx.nhs.uk
-CREATED:        15 Mar 2022
-VERSION:        0.0.1
+CREATED:        23 Aug 2022
+VERSION:        0.0.2
 """
 
 # COMMAND ----------
@@ -45,11 +45,11 @@ from azure.storage.filedatalake import DataLakeServiceClient
 # Connect to Azure datalake
 # -------------------------------------------------------------------------
 # !env from databricks secrets
-CONNECTION_STRING = dbutils.secrets.get(scope="datalakefs", key="CONNECTION_STRING")
+CONNECTION_STRING = dbutils.secrets.get(scope='AzureDataLake', key="DATALAKE_CONNECTION_STRING")
 
 # COMMAND ----------
 
-# MAGIC %run /Repos/prod/au-azure-databricks/functions/dbrks_helper_functions
+# MAGIC %run /Shared/databricks/au-azure-databricks-cicd/functions/dbrks_helper_functions
 
 # COMMAND ----------
 
@@ -57,20 +57,21 @@ CONNECTION_STRING = dbutils.secrets.get(scope="datalakefs", key="CONNECTION_STRI
 # -------------------------------------------------------------------------
 file_path_config = "/config/pipelines/nhsx-au-analytics/"
 file_name_config = "config_nhs_app_logins_dbrks.json"
-file_system_config = "nhsxdatalakesagen2fsprod"
+file_system_config = dbutils.secrets.get(scope='AzureDataLake', key="DATALAKE_CONTAINER_NAME")
 config_JSON = datalake_download(CONNECTION_STRING, file_system_config, file_path_config, file_name_config)
 config_JSON = json.loads(io.BytesIO(config_JSON).read())
 
 # COMMAND ----------
 
 #Get parameters from JSON config
-file_system = config_JSON['pipeline']['adl_file_system']
+file_system = dbutils.secrets.get(scope='AzureDataLake', key="DATALAKE_CONTAINER_NAME")
 source_path = config_JSON['pipeline']['project']['source_path']
 source_file = config_JSON['pipeline']['project']['source_file']
 sink_path = config_JSON['pipeline']['project']['databricks'][1]['sink_path']
 sink_file = config_JSON['pipeline']['project']['databricks'][1]['sink_file']  
 reference_source_path = config_JSON['pipeline']['project']['M041_reference_source_path']
 reference_source_file = config_JSON['pipeline']['project']['M041_reference_source_file']
+table_name = config_JSON['pipeline']["staging"][1]['sink_table']
 
 # COMMAND ----------
 
@@ -105,3 +106,9 @@ df_processed = df.copy()
 file_contents = io.StringIO()
 df_processed.to_csv(file_contents)
 datalake_upload(file_contents, CONNECTION_STRING, file_system, sink_path+latestFolder, sink_file)
+
+# COMMAND ----------
+
+# Write data from databricks to dev SQL database
+# -------------------------------------------------------------------------
+write_to_sql(df_processed, table_name, "overwrite")
--- orchestration/dbrks_nhs_app_logins_orchestrator.py
+++ orchestration/dbrks_nhs_app_logins_orchestrator.py
@@ -13,10 +13,10 @@ DESCRIPTION:
                 Orchestrator databricks notebook which runs the processing notebooks for NHSX Analyticus unit metrics for nhs app logins count
 USAGE:
                 ...
-CONTRIBUTORS:   Everistus Oputa
+CONTRIBUTORS:   Everistus Oputa, Mattia Ficarelli
 CONTACT:        data@nhsx.nhs.uk
-CREATED:        15 Mar. 2022
-VERSION:        0.0.1
+CREATED:        23 Aug. 2022
+VERSION:        0.0.2
 """
 
 # COMMAND ----------
@@ -45,28 +45,32 @@ from azure.storage.filedatalake import DataLakeServiceClient
 # Connect to Azure datalake
 # -------------------------------------------------------------------------
 # !env from databricks secrets
-CONNECTION_STRING = dbutils.secrets.get(scope="datalakefs", key="CONNECTION_STRING")
+CONNECTION_STRING = dbutils.secrets.get(scope="AzureDataLake", key="DATALAKE_CONNECTION_STRING") 
 
 # COMMAND ----------
 
-# MAGIC %run /Repos/prod/au-azure-databricks/functions/dbrks_helper_functions
+# MAGIC %run /Shared/databricks/au-azure-databricks-cicd/functions/dbrks_helper_functions
 
 # COMMAND ----------
 
 #Download JSON config from Azure datalake
 file_path_config = "/config/pipelines/nhsx-au-analytics/"
 file_name_config = "config_nhs_app_logins_dbrks.json"
-file_system_config = "nhsxdatalakesagen2fsprod"
+file_system_config = dbutils.secrets.get(scope="AzureDataLake", key="DATALAKE_CONTAINER_NAME")
 config_JSON = datalake_download(CONNECTION_STRING, file_system_config, file_path_config, file_name_config)
 config_JSON = json.loads(io.BytesIO(config_JSON).read())
 
 # COMMAND ----------
 
+#Get databricksworkspace specfic path
+#----------------------------------
+path_start = dbutils.secrets.get(scope='DatabricksNotebookPath', key="DATABRICKS_PATH")
+
 #Squentially run metric notebooks
 for index, item in enumerate(config_JSON['pipeline']['project']['databricks']): # get index of objects in JSON array
   try:
     notebook = config_JSON['pipeline']['project']['databricks'][index]['databricks_notebook']
-    dbutils.notebook.run(notebook, 1000) # is 120 sec long enough for timeout?
+    dbutils.notebook.run(path_start+notebook, 8000)
   except Exception as e:
     print(e)
     raise Exception()
