79bd93190675cf663ed8adc958ba7e13be8d1f93
mattia.ficarelli@nhsx.nhs.uk <mattia.ficarelli@nhsx.nhs.uk>
2022-08-26 10:11:27 +0000

    commit

--- analytics/dbrks_reference_odscodes/dbrks_odscodes_trust_mapping.py
+++ analytics/dbrks_reference_odscodes/dbrks_odscodes_trust_mapping.py
@@ -67,7 +67,8 @@ source_path = config_JSON['pipeline']['project']['source_path']
 source_file = config_JSON['pipeline']['project']['source_file']
 file_system = dbutils.secrets.get(scope='AzureDataLake', key="DATALAKE_CONTAINER_NAME")
 sink_path = config_JSON['pipeline']['project']['sink_path']
-sink_file = config_JSON['pipeline']['project']['sink_file']  
+sink_file = config_JSON['pipeline']['project']['sink_file']
+table_name = config_JSON['pipeline']["staging"]['sink_table']
 
 # COMMAND ----------
 
