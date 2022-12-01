64f507a9696808ff7130db6a5241c98477423a0e
Evop-hub <Evop-hub>
2022-12-01 15:48:34 +0000

    cqc_dscr_data
    commiting 3 notebooks for the cqc_dscr work

---
+++ orchestration/dbrks_dscr_orchestrator.py
@@ -0,0 +1,76 @@
+# Databricks notebook source
+#!/usr/bin python3
+
+# -------------------------------------------------------------------------
+# Copyright (c) 2021 NHS England and NHS Improvement. All rights reserved.
+# Licensed under the MIT License. See license.txt in the project root for
+# license information.
+# -------------------------------------------------------------------------
+
+"""
+FILE:          dbrks_dscr_orchestrator.py
+DESCRIPTION:
+                Orchestrator databricks notebook which runs the processing notebooks for NHSX Analyticus unit metrics within the CQC Digital Social Care Record (DSCRs) topic
+USAGE:
+                ...
+CONTRIBUTORS:   Everistus Oputa
+CONTACT:        data@nhsx.nhs.uk
+CREATED:        07 Nov. 2022
+VERSION:        0.0.1
+"""
+
+# COMMAND ----------
+
+# Install libs
+# -------------------------------------------------------------------------
+%pip install geojson==2.5.* tabulate requests pandas pathlib azure-storage-file-datalake beautifulsoup4 numpy urllib3 lxml regex pyarrow==5.0.*
+
+# COMMAND ----------
+
+# Imports
+# -------------------------------------------------------------------------
+# Python:
+import os
+import io
+import tempfile
+from datetime import datetime
+import json
+
+# 3rd party:
+import pandas as pd
+import numpy as np
+from pathlib import Path
+from azure.storage.filedatalake import DataLakeServiceClient
+
+# Connect to Azure datalake
+# -------------------------------------------------------------------------
+# !env from databricks secrets
+CONNECTION_STRING = dbutils.secrets.get(scope='AzureDataLake', key="DATALAKE_CONNECTION_STRING")
+
+# COMMAND ----------
+
+# MAGIC %run /Shared/databricks/au-azure-databricks-cicd/functions/dbrks_helper_functions
+
+# COMMAND ----------
+
+#Download JSON config from Azure datalake
+file_path_config = "/config/pipelines/nhsx-au-analytics/"
+file_name_config = "config_dscr_dbrks.json"
+file_system_config = dbutils.secrets.get(scope="AzureDataLake", key="DATALAKE_CONTAINER_NAME")
+config_JSON = datalake_download(CONNECTION_STRING, file_system_config, file_path_config, file_name_config)
+config_JSON = json.loads(io.BytesIO(config_JSON).read())
+
+# COMMAND ----------
+
+#Get databricksworkspace specfic path
+#---------------------------------
+path_start = dbutils.secrets.get(scope='DatabricksNotebookPath', key="DATABRICKS_PATH")
+
+#Squentially run metric notebooks
+for index, item in enumerate(config_JSON['pipeline']['project']['databricks']): # get index of objects in JSON array
+  try:
+    notebook = config_JSON['pipeline']['project']['databricks'][index]['databricks_notebook']
+    dbutils.notebook.run(path_start+notebook, 8000) # is 120 sec long enough for timeout?
+  except Exception as e:
+    print(e)
+    raise Exception()
