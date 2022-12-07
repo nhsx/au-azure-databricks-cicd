de867074673bb9438963ca61b0838fa1b76197d3
abdu-r <abdu-r>
2022-12-07 14:12:58 +0000

    Add PIR data

--- ingestion/dbrks_dscr/dbrks_dscr_results_raw.py
+++ ingestion/dbrks_dscr/dbrks_dscr_results_raw.py
@@ -87,14 +87,7 @@ file_name_list = datalake_listContents(CONNECTION_STRING, file_system, new_sourc
 file_name_list = [file for file in file_name_list if '.csv' in file]
 for new_source_file in file_name_list:
   new_dataset = datalake_download(CONNECTION_STRING, file_system, new_source_path+latestFolder, new_source_file)
-  header_list = ["Location ID","Dormant (Y/N)","Care home?","Location Inspection Directorate","Location Primary Inspection Category","Location Local Authority","Location ONSPD CCG Code","Location ONSPD CCG","Provider ID","Provider Inspection Directorate","Provider Primary Inspection Category","Provider Postal Code", "run_date"]
-  #new_date = datetime.now().strftime('%Y-%m-%d') + '/' 
   new_dataframe = pd.read_csv(io.BytesIO(new_dataset), encoding = "ISO-8859-1")
-  df2 = new_dataframe[header_list]  
-  #df2 = new_dataframe.loc[:, ~new_dataframe.columns.str.contains('^Unnamed')]
-  #df2['run_date'] = latestFolder.replace('/','')
-  #df2['run_date'] = pd.to_datetime(df2['run_date']).dt.strftime('%Y-%m-%d')
-  #df2["refreshed_date"] = datetime.now().strftime('%Y-%m-%d')   
 
 
 # COMMAND ----------
@@ -104,15 +97,14 @@ for new_source_file in file_name_list:
 latestFolder = datalake_latestFolder(CONNECTION_STRING, file_system, historical_source_path)
 historical_dataset = datalake_download(CONNECTION_STRING, file_system, historical_source_path+latestFolder, historical_source_file)
 historical_dataframe = pd.read_parquet(io.BytesIO(historical_dataset), engine="pyarrow")
-historical_dataframe['run_date'] = pd.to_datetime(historical_dataframe['run_date']).dt.strftime('%Y-%m')
 
 # COMMAND ----------
 
 # Append new data to historical data
 # -----------------------------------------------------------------------
-date_from_new_dataframe = df2["run_date"].values.max()
+date_from_new_dataframe = new_dataframe["run_date"].values.max()
 if date_from_new_dataframe != historical_dataframe['run_date'].values.max():
-  historical_dataframe = historical_dataframe.append(df2)
+  historical_dataframe = historical_dataframe.append(new_dataframe)
   historical_dataframe = historical_dataframe.sort_values(by=['run_date'])
   historical_dataframe = historical_dataframe.reset_index(drop=True)
   historical_dataframe = historical_dataframe.astype(str)
@@ -123,6 +115,7 @@ else:
 
 # Upload hsitorical appended data to datalake
 current_date_path = datetime.now().strftime('%Y-%m-%d') + '/'
+new_dataframe['Provider Charity Number'] = pd.to_numeric(new_dataframe['Provider Charity Number'], errors='coerce')
 file_contents = io.BytesIO()
 historical_dataframe.to_parquet(file_contents, engine="pyarrow")
 datalake_upload(file_contents, CONNECTION_STRING, file_system, sink_path+current_date_path, sink_file)
