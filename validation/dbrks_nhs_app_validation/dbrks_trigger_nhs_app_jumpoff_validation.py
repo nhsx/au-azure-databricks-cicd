01bfb5523c1e96f6f3ce36a462c364caf6456698
faaiz-25 <faaiz-25>
2023-01-23 16:48:53 +0000

    changing jumpoff validation to 1%

--- validation/dbrks_nhs_app_validation/dbrks_trigger_nhs_app_jumpoff_validation.py
+++ validation/dbrks_nhs_app_validation/dbrks_trigger_nhs_app_jumpoff_validation.py
@@ -131,7 +131,7 @@ previous_ods_count = get_last_agg(agg_log_table, 'WeeklyIntegratedPartners', "co
 display(previous_ods_count)
 
 #calculate minimum and maximum tolerance values (within 1%)
-min_ods_count, max_ods_count = get_thresholds(previous_ods_count['aggregate_value'].values[0], 10)
+min_ods_count, max_ods_count = get_thresholds(previous_ods_count['aggregate_value'].values[0], 1)
 
 # COMMAND ----------
 
