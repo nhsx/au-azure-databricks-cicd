ab1cc446e8a5d3cbddd80b83244bd0303b03fbee
mskabirkhan <mskabirkhan>
2023-02-02 16:34:12 +0000

    Added ingestion code, need more testing

--- ingestion/dbrks_dscr_ics_qrtly_report/dbrks_dscr_ics_qrtly_report_raw.py
+++ ingestion/dbrks_dscr_ics_qrtly_report/dbrks_dscr_ics_qrtly_report_raw.py
@@ -88,16 +88,8 @@ file_name_list = datalake_listContents(CONNECTION_STRING, file_system, new_sourc
 file_name_list = [file for file in file_name_list if '.xlsm' in file]
 for new_source_file in file_name_list:
   new_dataset = datalake_download(CONNECTION_STRING, file_system, new_source_path+latestFolder, new_source_file)
-  new_dataframe = pd.read_excel(io.BytesIO(new_dataset), sheet_name = "Backsheet for Pipeline", header = 1, engine='openpyxl') 
+  new_dataframe = pd.read_excel(io.BytesIO(new_dataset), sheet_name = "Backsheet for Pipeline", header = 0, engine='openpyxl') 
  
-
-# COMMAND ----------
-
-new_source_file
-
-# COMMAND ----------
-
-new_dataframe
 
 # COMMAND ----------
 
@@ -106,73 +98,52 @@ new_dataframe
 latestFolder_historical = datalake_latestFolder(CONNECTION_STRING, file_system, historical_source_path)
 historical_dataset = datalake_download(CONNECTION_STRING, file_system, historical_source_path+latestFolder_historical, historical_source_file)
 historical_dataframe = pd.read_parquet(io.BytesIO(historical_dataset), engine="pyarrow")
-historical_dataframe
 
 
 # COMMAND ----------
 
-#drop rows that contain no data in any of the columns
-df_process = new_dataframe.copy()
-df_process.dropna(axis = 0, how = 'all', inplace = True)
-df_process = df_process.reset_index()
-df_process
+# Getting ics_overview row from both dataframe to compare if data is exists
+ics_new_dataframe = new_dataframe[['ICS Overview - Year 1 2022/23 Reporting QTR:',
+       'ICS Overview  - ICS NAME:', 'ICS Overview - LOCAL AUTHORITY NAME:',
+       'ICS Overview - ICS SRO Approved prior to submission:',
+       'ICS Overview - APPROVED BY (Name):', 'ICS Overview - (Job Role):',
+       'ICS Overview - Submitted to DiSC By (Name): ',
+       'ICS Overview - (Job Role):.1',
+       'ICS Overview  - Date of Qtrly Report Submission:',
+       'ICS Overview  - DiSC Regional Lead Approved:',
+       'ICS Overview - Date Reviewed :',
+       'ICS Overview - QTRLY FUNDING ALLOCATION:',
+       'ICS Overview - QTRLY FUNDING  APPROVED: ',
+       'ICS Overview - ESCALATION REQUIRED:',
+       'ICS Overview - DATE ESCALATION MTG:']].iloc[0]
 
-
-# COMMAND ----------
-
-##process new dataframe
-
-#drop rows that contain no data in any of the columns
-df_process = new_dataframe.copy()
-df_process.dropna(axis = 0, how = 'all', inplace = True)
-df_process.reset_index(inplace = True)
-
-#copy ICS overview information for each row
-ics_row = df_process[['ICS Overview - Year 1 2022/23 Reporting QTR:',
- 'ICS Overview  - ICS NAME:',
- 'ICS Overview - LOCAL AUTHORITY NAME:',
- 'ICS Overview - ICS SRO Approved prior to submission:',
- 'ICS Overview - APPROVED BY (Name):',
- 'ICS Overview - (Job Role):',
- 'ICS Overview - Submitted to DiSC By (Name): ',
- 'ICS Overview - (Job Role):.1',
- 'ICS Overview  - Date of Qtrly Report Submission:',
- 'ICS Overview  - DiSC Regional Lead Approved:',
- 'ICS Overview - Date Reviewed :',
- 'ICS Overview - QTRLY FUNDING ALLOCATION:',
- 'ICS Overview - QTRLY FUNDING  APPROVED: ',
- 'ICS Overview - ESCALATION REQUIRED:',
- 'ICS Overview - DATE ESCALATION MTG:']].iloc[0]
-
-for i in range(df_process.shape[0]):
-  df_process.loc[i, ['ICS Overview - Year 1 2022/23 Reporting QTR:',
- 'ICS Overview  - ICS NAME:',
- 'ICS Overview - LOCAL AUTHORITY NAME:',
- 'ICS Overview - ICS SRO Approved prior to submission:',
- 'ICS Overview - APPROVED BY (Name):',
- 'ICS Overview - (Job Role):',
- 'ICS Overview - Submitted to DiSC By (Name): ',
- 'ICS Overview - (Job Role):.1',
- 'ICS Overview  - Date of Qtrly Report Submission:',
- 'ICS Overview  - DiSC Regional Lead Approved:',
- 'ICS Overview - Date Reviewed :',
- 'ICS Overview - QTRLY FUNDING ALLOCATION:',
- 'ICS Overview - QTRLY FUNDING  APPROVED: ',
- 'ICS Overview - ESCALATION REQUIRED:',
- 'ICS Overview - DATE ESCALATION MTG:']] = ics_row
-  
-df_process
+ics_historical_dataframe = historical_dataframe[['ICS Overview - Year 1 2022/23 Reporting QTR:',
+       'ICS Overview  - ICS NAME:', 'ICS Overview - LOCAL AUTHORITY NAME:',
+       'ICS Overview - ICS SRO Approved prior to submission:',
+       'ICS Overview - APPROVED BY (Name):', 'ICS Overview - (Job Role):',
+       'ICS Overview - Submitted to DiSC By (Name): ',
+       'ICS Overview - (Job Role):.1',
+       'ICS Overview  - Date of Qtrly Report Submission:',
+       'ICS Overview  - DiSC Regional Lead Approved:',
+       'ICS Overview - Date Reviewed :',
+       'ICS Overview - QTRLY FUNDING ALLOCATION:',
+       'ICS Overview - QTRLY FUNDING  APPROVED: ',
+       'ICS Overview - ESCALATION REQUIRED:',
+       'ICS Overview - DATE ESCALATION MTG:']]
 
 # COMMAND ----------
 
-#drop the separator columns (columns which are unnamed)
-df_process = df_process[df_process.columns.drop(list(df_process.filter(regex='Unnamed')))]
-df_process
+# comparing if new data already exists in historical data 
 
-# COMMAND ----------
-
-historical_dataframe = historical_dataframe.append(df_process)
-#historical_dataframe
+exists = False
+for i in range(0, ics_historical_dataframe.shape[0]):
+  if (ics_historical_dataframe.iloc[i] == ics_new_dataframe).all() == True:
+    exists = True
+    
+if exists == True:
+  print('Data already exists in historical data')
+else:
+  historical_dataframe = historical_dataframe.append(new_dataframe)
 
 # COMMAND ----------
 
