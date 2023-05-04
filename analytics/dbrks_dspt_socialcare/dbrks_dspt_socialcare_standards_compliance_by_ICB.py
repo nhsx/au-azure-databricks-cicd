8d50f89f85cab13d532b9c404d2fd2d95a670242
Evop-hub <Evop-hub>
2023-05-04 13:51:40 +0000

    Dspt_Social_Care_Updated
    Dspt Social Care  latest woking and updates

--- analytics/dbrks_dspt_socialcare/dbrks_dspt_socialcare_standards_compliance_by_ICB.py
+++ analytics/dbrks_dspt_socialcare/dbrks_dspt_socialcare_standards_compliance_by_ICB.py
@@ -13,17 +13,17 @@ DESCRIPTION:
                 Databricks notebook with processing code for the NHSX Analyticus unit metric: Number and percent of adult social care organisations that meet or exceed the DSPT standard (M011 & M012)
 USAGE:
                 ...
-CONTRIBUTORS:   Craig Shenton, Mattia Ficarelli
+CONTRIBUTORS:   Craig Shenton, Mattia Ficarelli, Faaiz Muhammad , Everistus Oputa
 CONTACT:        data@nhsx.nhs.uk
-CREATED:        22 Aug 2021
-VERSION:        0.0.2
+CREATED:        04 May 2023
+VERSION:        0.0.3
 """
 
 # COMMAND ----------
 
 # Install libs
 # -------------------------------------------------------------------------
-%pip install geojson==2.5.* tabulate requests pandas pathlib azure-storage-file-datalake beautifulsoup4 numpy urllib3 lxml regex pyarrow==5.0.*
+%pip install geojson==2.5.* tabulate requests pandas pathlib azure-storage-file-datalake beautifulsoup4 numpy urllib3 lxml regex pyarrow==8.0.*
 
 # COMMAND ----------
 
@@ -82,11 +82,13 @@ table_name = config_JSON['pipeline']["staging"][1]['sink_table']
 
 #Get parameters from JSON config
 # -------------------------------------------------------------------------
-dscr_source_path = dscr_config_JSON['pipeline']['project']['source_path']
-dscr_source_file = dscr_config_JSON['pipeline']['project']['source_file']
+dscr_source_path = dscr_config_JSON['pipeline']['raw']['snapshot_source_path']
+dscr_source_file = dscr_config_JSON['pipeline']['raw']['appended_file']
 dscr_reference_path = dscr_config_JSON['pipeline']['project']['reference_source_path']
 dscr_reference_file = dscr_config_JSON['pipeline']['project']['reference_source_file']
 dscr_file_system = dbutils.secrets.get(scope='AzureDataLake', key="DATALAKE_CONTAINER_NAME")
+#dscr_sink_path = dscr_config_JSON['pipeline']['project']['databricks'][1]['sink_path']
+#dscr_sink_file = dscr_config_JSON['pipeline']['project']['databricks'][1]['sink_file']
 dscr_sink_path = dscr_config_JSON['pipeline']['project']['databricks'][1]['sink_path']
 dscr_sink_file = dscr_config_JSON['pipeline']['project']['databricks'][1]['sink_file']
 reference_path = dscr_config_JSON['pipeline']['project']['reference_source_path']
@@ -101,13 +103,13 @@ latestFolder = datalake_latestFolder(CONNECTION_STRING, file_system, source_path
 file = datalake_download(CONNECTION_STRING, file_system, source_path+latestFolder, source_file)
 df = pd.read_parquet(io.BytesIO(file), engine="pyarrow")
 
-# COMMAND ----------
 
-df.columns
-
-# COMMAND ----------
-
-df
+latestFolder = datalake_latestFolder(CONNECTION_STRING, file_system, dscr_source_path)
+file_name_list = datalake_listContents(CONNECTION_STRING, file_system, dscr_source_path+latestFolder)
+file_name_list = [file for file in file_name_list if '.csv' in file]
+for new_source_file in file_name_list:
+  new_dataset = datalake_download(CONNECTION_STRING, file_system, dscr_source_path+latestFolder, new_source_file)
+  new_dataframe = pd.read_csv(io.BytesIO(new_dataset), encoding = "ISO-8859-1")
 
 # COMMAND ----------
 
@@ -116,28 +118,28 @@ df
 dscr_latestFolder = datalake_latestFolder(CONNECTION_STRING, dscr_file_system, dscr_source_path)
 ref_latestFolder = datalake_latestFolder(CONNECTION_STRING, dscr_file_system, reference_path)
 
-dscr_file = datalake_download(CONNECTION_STRING, dscr_file_system, dscr_source_path+dscr_latestFolder, dscr_source_file)
-df_dscr = pd.read_parquet(io.BytesIO(dscr_file), engine="pyarrow")
+dscr_file = datalake_latestFolder(CONNECTION_STRING, file_system, dscr_source_path)
+file_name_list = datalake_listContents(CONNECTION_STRING, file_system, dscr_source_path+latestFolder)
+file_name_list = [file for file in file_name_list if '.csv' in file]
+for new_source_file in file_name_list:
+  new_dataset = datalake_download(CONNECTION_STRING, file_system, dscr_source_path+latestFolder, new_source_file)
+  new_dataframe = pd.read_csv(io.BytesIO(new_dataset), encoding = "ISO-8859-1")
 
 ref_file = datalake_download(CONNECTION_STRING, dscr_file_system, reference_path+ref_latestFolder, reference_file)
 df_ref = pd.read_parquet(io.BytesIO(ref_file), engine='pyarrow')
 
 # COMMAND ----------
 
-df_ref
-
-# COMMAND ----------
-
 #get CCG ONS code and CQC-location-id
-df_CCG = df_dscr[['Location ID', 'Location ONSPD CCG Code']]
+df_CCG = new_dataframe[['Location ID', 'Location ONSPD CCG Code']]
 df_CCG = df_CCG.rename(columns={'Location ID': 'Location CQC ID ', 'Location ONSPD CCG Code':'CCG_ONS_Code'})
 df_CCG = df_CCG.merge(df_ref, on='CCG_ONS_Code', how='left')
-df_CCG
+#df_CCG
 
 # COMMAND ----------
 
 df_join = df.merge(df_CCG, on = 'Location CQC ID ', how = 'left')
-df_join
+#df_join
 
 # COMMAND ----------
 
@@ -152,95 +154,59 @@ df = df_join
 df["Count"] = 1
 
 
-df_1 = df.groupby(['Date',"CQC registered location - latest DSPT status", 'ICB_Code']).sum().reset_index()
+#df_1 = df.groupby(['Date',"CQC registered location - latest DSPT status", 'ICB_Code']).sum().reset_index()
 
 
-#Makes changed as outlined in the SOP when the finanical year flag for Standards Met/Exceeded changes
-#----------------------------------------------------------------------------------------------------
-#19/20 and 20/21 FY
-#-----------------------------------
-df_2 = df_1.loc[df_1['Date'] <= '2021-09']
-df_3 = df_2[
-(df_2["CQC registered location - latest DSPT status"] == "19/20 Standards Met.") |
-(df_2["CQC registered location - latest DSPT status"] == "19/20 Standards Exceeded.") |
-(df_2["CQC registered location - latest DSPT status"] == "20/21 Standards Met.") |
-(df_2["CQC registered location - latest DSPT status"] == "20/21 Standards Exceeded.")
-].reset_index(drop=True)
-df_4 = df_3.groupby(["Date", "CQC registered location - latest DSPT status", 'ICB_Code']).sum().reset_index()
-
-icb_list = list(df_4['ICB_Code'].unique())
-for icb in icb_list:
-  total = df_4.loc[df_4['ICB_Code'] == icb]['Count'].sum()
-  df_4.loc[df_4['ICB_Code'] == icb, 'Total'] = total
-  
-
-#20/21 and 21/22 FY
-#---------------------------------
-df_5 = df_1.loc[(df_1['Date'] >= '2021-10') & (df_1['Date'] < '2022-09')] #------------- change filter once new finanical year added.
-df_6 = df_5[
-(df_5["CQC registered location - latest DSPT status"] == "20/21 Standards Met.") |
-(df_5["CQC registered location - latest DSPT status"] == "20/21 Standards Exceeded.") |
-(df_5["CQC registered location - latest DSPT status"] == "21/22 Standards Met.") |
-(df_5["CQC registered location - latest DSPT status"] == "21/22 Standards Exceeded.")
-].reset_index(drop=True)
-df_7 = df_6.groupby(["Date", "CQC registered location - latest DSPT status", 'ICB_Code']).sum().reset_index()
-
-icb_list = list(df_7['ICB_Code'].unique())
-for icb in icb_list:
-  total = df_7.loc[df_7['ICB_Code'] == icb]['Count'].sum()
-  df_7.loc[df_7['ICB_Code'] == icb, 'Total'] = total
-
-#21/22 and 22/23 FY
-#---------------------------------
-df_8 = df_1.loc[df_1['Date'] >= '2022-09']
-df_9 = df_8[
-(df_8["CQC registered location - latest DSPT status"] == "21/22 Standards Met.") |
-(df_8["CQC registered location - latest DSPT status"] == "21/22 Standards Exceeded.") |
-(df_8["CQC registered location - latest DSPT status"] == "22/23 Standards Met.") |
-(df_8["CQC registered location - latest DSPT status"] == "22/23 Standards Exceeded.")
-].reset_index(drop=True)
-df_10 = df_9.groupby(["Date", "CQC registered location - latest DSPT status", 'ICB_Code']).sum().reset_index()
-
-icb_list = list(df_10['ICB_Code'].unique())
-for icb in icb_list:
-  total = df_10.loc[df_10['ICB_Code'] == icb]['Count'].sum()
-  df_10.loc[df_10['ICB_Code'] == icb, 'Total'] = total
-
-#---------------------------------------------------------------------------------------
-df_fy_appended = pd.concat([df_4, df_7, df_10]).reset_index(drop = True) #------------- Once a new finanical year is added appened additional FY dataframe. ie pd.concat([df_4, df_7, df_10]).reset_index(drop = True)
-#---------------------------------------------------------------------------------------
-#df_dates = df_1.groupby("Date").sum().reset_index()
-#df_dates_merged = df_fy_appended.merge(df_dates, on = 'Date', how = 'left')
-#df_dates_merged_1 = df_dates_merged.rename(columns = { 'Count_x':'Number of social care organizations with a standards met or exceeded DSPT status', 'Count_y':'Total number of social care organizations'})
-#df_dates_merged_1["Percent of social care organizations with a standards met or exceeded DSPT status"] = df_dates_merged_1['Number of social care organizations with a standards met or exceeded DSPT status']/df_dates_merged_1['Total number of social care organizations']
-#df_dates_merged_2 = df_dates_merged_1.round(4)
-#df_dates_merged_2["Date"] = pd.to_datetime(df_dates_merged_2["Date"])
-#df_dates_merged_2.index.name = "Unique ID"
-#df_processed = df_dates_merged_2.copy()
-
-df_fy_appended['Date'] = pd.to_datetime(df_fy_appended['Date'])
-df_processed = df_fy_appended
 
 # COMMAND ----------
 
-#21/22 and 22/23 FY
-#---------------------------------
-df_latest = df_1.loc[df_1['Date'] >= '2022-09']
-df_latest = df_latest .groupby(["Date", "CQC registered location - latest DSPT status", 'ICB_Code']).sum().reset_index()
 
-icb_list = list(df_latest['ICB_Code'].unique())
-for icb in icb_list:
-  total = df_latest.loc[df_latest['ICB_Code'] == icb]['Count'].sum()
-  df_latest.loc[df_latest['ICB_Code'] == icb, 'Total'] = total
+#uncomment this if on 21/22/ and 22/23 status are needed 
+#---------------------------------------------------------------------------------------------------------------------------------------
+# df = df[df["CQC registered location - latest DSPT status"].isin(["21/22 Approaching Standards.", 
+#                                                                    "21/22 Standards Exceeded.", 
+#                                                                    "21/22 Standards Met.", 
+#                                                                    "21/22 Standards Not Met.",
+#                                                                    "22/23 Approaching Standards.",
+#                                                                    "22/23 Standards Exceeded.",
+#                                                                    "22/23 Standards Me.",
+#                                                                    "Not Individually Registered.",                                                     
+#                                                                    "Not Published." ])].reset_index(drop=True) #------ select required FY for the DSPT standard 
 
-df_latest['Total'] = df_latest['Total'].astype(int)
-df_latest['CQC registered location - latest DSPT status'] = df_latest['CQC registered location - latest DSPT status'].str.replace('.', '')
+                                                                  
 
-df_latest['Date'] = pd.to_datetime(df_latest['Date'])
-df_latest = df_latest.rename(columns = {'Date':'Report Date', 'CQC registered location - latest DSPT status':'Standard status', 'Count':'Number of locations with the standard status', 'Total':'Total number of locations'})
+# Generating Number of Social Care Organisation Dspt Status
+df_latest = df[["Date","CQC registered location - latest DSPT status","ICB_Code"]].copy()
+df_latest = df.loc[df['Date'] >= '2022-09']
+df_latest['CQC registered location - latest DSPT status'] = df_latest['CQC registered location - latest DSPT status'].astype(str)
+df_latest = df_latest.groupby(['Date','ICB_Code','CQC registered location - latest DSPT status'], as_index=False).size()
+df5 = df_latest[['Date','ICB_Code','CQC registered location - latest DSPT status','size']]
+#df6 = df5.rename(columns = {'Date': 'Date','ICB_Code':'ICB_Code','CQC registered location - latest DSPT status':'ICB_Code':'CQC registered location - latest DSPT status','size':'Total'})
 
-df_latest
 
+# Generating Total number of socialcare organisation per ICB by date
+df_latest = df[["Date", "ICB_Code"]].copy()
+df_latest = df.loc[df['Date'] >= '2022-09']
+df_latest['ICB_Code'] = df_latest['ICB_Code'].astype(str)
+df_latest = df_latest.groupby(['Date','ICB_Code'], as_index=False).size()
+df2 = df_latest[['Date','ICB_Code','size']]
+df3 = df2.rename(columns = {'Date': 'Date','ICB_Code':'ICB_Code','size':'Total'})
+
+
+#Joined data processing for social care dspt status and Total number social care organisation
+df_join = pd.merge(df3,df5, on = ['ICB_Code','Date']) 
+df_join_1 = df_join.rename(columns = {'Date':'Report Date','ICB_Code': 'ICB_CODE','CQC registered location - latest DSPT status': 'Standard status','size':'Number of locations with the standard status','Total':'Total number of locations'})
+# df_join_1["Percent of Trusts with a standards met or exceeded DSPT status"] = df_join_1["Number of Trusts with the standard status"]/df_join_1["Total number of Trusts"]  (apply similar formular for social care if the percentage is needed as well)
+df_join_1 = df_join_1.round(2)
+#df_join_1['Report Date'] = pd.to_datetime(df_join_1['Report Date'])
+df_join_1.index.name = "Unique ID"
+df_processed = df_join_1.copy()
+
+
+
+# COMMAND ----------
+
+display(df_processed )
 
 # COMMAND ----------
 
@@ -256,7 +222,3 @@ datalake_upload(file_contents, CONNECTION_STRING, file_system, sink_path+current
 # Write data from databricks to dev SQL database
 # -------------------------------------------------------------------------
 write_to_sql(df_latest, table_name, "overwrite")
-
-# COMMAND ----------
-
-
