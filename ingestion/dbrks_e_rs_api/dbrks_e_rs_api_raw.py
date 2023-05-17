187263b641c52fdee21a721fd87c5f0667b302cc
faaiz-25 <faaiz-25>
2023-05-17 16:02:15 +0000

    fixing encoding issue

--- ingestion/dbrks_e_rs_api/dbrks_e_rs_api_raw.py
+++ ingestion/dbrks_e_rs_api/dbrks_e_rs_api_raw.py
@@ -98,7 +98,7 @@ for new_source_file in file_name_list:
 latestFolder = datalake_latestFolder(CONNECTION_STRING, file_system, historical_source_path)
 historical_dataset = datalake_download(CONNECTION_STRING, file_system, historical_source_path+latestFolder, historical_source_file)
 # historical_dataframe = pd.read_csv(io.BytesIO(historical_dataset))
-historical_dataframe =  pd.read_csv(io.BytesIO(historical_dataset), encoding = "ISO-8859-1")
+historical_dataframe =  pd.read_csv(io.BytesIO(historical_dataset))
 historical_dataframe['Report_End _Date'] = pd.to_datetime(historical_dataframe['Report_End _Date']).dt.strftime('%Y-%m-%d')
 
 # Append new data to historical data
@@ -114,8 +114,12 @@ else:
 
 # COMMAND ----------
 
+historical_dataframe
+
+# COMMAND ----------
+
 # Upload hsitorical appended data to datalake
 current_date_path = datetime.now().strftime('%Y-%m-%d') + '/'
-file_contents = io.StringIO()
+file_contents = io.BytesIO()
 historical_dataframe.to_csv(file_contents, index=False)
 datalake_upload(file_contents, CONNECTION_STRING, file_system, sink_path+current_date_path, sink_file)
