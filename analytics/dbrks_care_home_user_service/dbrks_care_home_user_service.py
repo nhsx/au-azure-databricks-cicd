ed301c9e4880a1a72c61c377ce0d2201064b3bdf
faaiz-25 <faaiz-25>
2023-09-14 14:58:11 +0000

    creatd config file for care home users and modified home care user service ingestion

---
+++ analytics/dbrks_care_home_user_service/dbrks_care_home_user_service.py
@@ -0,0 +1,97 @@
+# Databricks notebook source
+# -------------------------------------------------------------------------
+# Copyright (c) 2021 NHS England and NHS Improvement. All rights reserved.
+# Licensed under the MIT License. See license.txt in the project root for
+# license information.
+# -------------------------------------------------------------------------
+
+"""
+FILE:          dbrks_home_care_user_service.py
+DESCRIPTION:
+                
+               Databricks notebook with processing home care service users
+USAGE:
+                ...
+CONTRIBUTORS:   Kabir Khan
+CONTACT:        data@nhsx.nhs.uk
+CREATED:        23 Feb 2023
+VERSION:        0.0.1
+"""
+
+# COMMAND ----------
+
+# Install libs
+# -------------------------------------------------------------------------
+%pip install geojson==2.5.* tabulate requests pandas pathlib azure-storage-file-datalake beautifulsoup4 numpy urllib3 lxml regex pyarrow==5.0.*
+
+# COMMAND ----------
+
+# Imports
+# -------------------------------------------------------------------------
+# Python:
+import os
+import io
+import tempfile
+from datetime import datetime
+import json
+
+# 3rd party:
+import pandas as pd
+import numpy as np
+from pathlib import Path
+from azure.storage.filedatalake import DataLakeServiceClient
+
+# Connect to Azure datalake
+# -------------------------------------------------------------------------
+# !env from databricks secrets
+CONNECTION_STRING = dbutils.secrets.get(scope='AzureDataLake', key="DATALAKE_CONNECTION_STRING")
+
+# COMMAND ----------
+
+# MAGIC %run /Shared/databricks/au-azure-databricks-cicd/functions/dbrks_helper_functions
+
+# COMMAND ----------
+
+# Load JSON config from Azure datalake
+# -------------------------------------------------------------------------
+file_path_config = "/config/pipelines/nhsx-au-analytics/"
+file_name_config = "config_home_care_user_service.json"
+file_system_config = dbutils.secrets.get(scope='AzureDataLake', key="DATALAKE_CONTAINER_NAME")
+config_JSON = datalake_download(CONNECTION_STRING, file_system_config, file_path_config, file_name_config)
+config_JSON = json.loads(io.BytesIO(config_JSON).read())
+
+# COMMAND ----------
+
+# Read parameters from JSON config
+# -------------------------------------------------------------------------
+file_system = dbutils.secrets.get(scope="AzureDataLake", key="DATALAKE_CONTAINER_NAME")
+source_path = config_JSON['pipeline']['project']['source_path']
+source_file = config_JSON['pipeline']['project']['source_file']
+sink_path = config_JSON['pipeline']['project']['sink_path']
+sink_file = config_JSON['pipeline']['project']['sink_file']
+table_name = config_JSON['pipeline']['staging'][0]['sink_table']
+
+# COMMAND ----------
+
+# Processing Data
+# -------------------------------------------------------------------------
+latestFolder = datalake_latestFolder(CONNECTION_STRING, file_system, source_path)
+file = datalake_download(CONNECTION_STRING, file_system, source_path+latestFolder, source_file)
+df = pd.read_parquet(io.BytesIO(file), engine="pyarrow")
+
+# Convert the 'Date' column to datetime format
+#df['Date']=pd.to_datetime(df["Date"],unit='s')
+
+# COMMAND ----------
+
+# Upload processed data to datalake
+# -------------------------------------------------------------------------
+file_contents = io.StringIO()
+df.to_csv(file_contents)
+datalake_upload(file_contents, CONNECTION_STRING, file_system, sink_path+latestFolder, sink_file)
+
+# COMMAND ----------
+
+# Write data from databricks to dev SQL database
+# -------------------------------------------------------------------------
+write_to_sql(df, table_name, "overwrite")
---
+++ validation/dbrks_care_home_user_service/dbrks_care_home_user_service_validation.py
@@ -0,0 +1,138 @@
+# Databricks notebook source
+#!/usr/bin python3
+
+# -------------------------------------------------------------------------
+# Copyright (c) 2021 NHS England and NHS Improvement. All rights reserved.
+# Licensed under the MIT License. See license.txt in the project root for
+# license information.
+# -------------------------------------------------------------------------
+
+"""
+FILE:           dbrks_home_care_user_service_count_validation.py
+DESCRIPTION:
+                To reduce manual checks and improve quality validations need to added to dbrks_home_care_user_service_count_validation pipeline for digital social care
+USAGE:
+                ...
+CONTRIBUTORS:   Kabir Khan
+CONTACT:        nhsx.data@england.nhs.uk
+CREATED:        28 Feb 2023
+VERSION:        0.0.1
+"""
+
+# COMMAND ----------
+
+# Install libs
+# -------------------------------------------------------------------------------------
+%pip install pandas pathlib azure-storage-file-datalake numpy pyarrow==5.0.* great_expectations openpyxl
+
+# COMMAND ----------
+
+# Imports
+# -------------------------------------------------------------------------
+# Python:
+import os
+import io
+import tempfile
+from datetime import datetime
+import json
+
+# 3rd party:
+import pandas as pd
+import numpy as np
+from pathlib import Path
+from azure.storage.filedatalake import DataLakeServiceClient
+import great_expectations as ge
+
+# Connect to Azure datalake
+# -------------------------------------------------------------------------
+# !env from databricks secrets
+CONNECTION_STRING = dbutils.secrets.get(scope='AzureDataLake', key="DATALAKE_CONNECTION_STRING")
+
+# COMMAND ----------
+
+# MAGIC %run /Shared/databricks/au-azure-databricks-cicd/functions/dbrks_helper_functions
+
+# COMMAND ----------
+
+# Load parameters and JSON config from Azure datalake
+# -------------------------------------------------------------------------
+
+file_path_config = "config/pipelines/nhsx-au-analytics"
+file_name_config = "config_home_care_user_service.json"
+log_table = "dbo.pre_load_log"
+
+file_system_config = dbutils.secrets.get(scope="AzureDataLake", key="DATALAKE_CONTAINER_NAME")
+config_JSON = datalake_download(CONNECTION_STRING, file_system_config, file_path_config, file_name_config)
+config_JSON = json.loads(io.BytesIO(config_JSON).read())
+
+# COMMAND ----------
+
+# Read parameters from JSON config
+# -------------------------------------------------------------------------
+file_system = dbutils.secrets.get(scope="AzureDataLake", key="DATALAKE_CONTAINER_NAME")
+new_source_path = config_JSON["pipeline"]['raw']["snapshot_source_path"]
+
+# COMMAND ----------
+
+# Pull new dataset
+# -------------------------
+latestFolder = datalake_latestFolder(CONNECTION_STRING, file_system, new_source_path)
+file_name_list = datalake_listContents(CONNECTION_STRING, file_system, new_source_path+ latestFolder)
+file_name_list = [file for file in file_name_list if '.xlsx' in file]
+for new_source_file in file_name_list:
+  new_dataset = datalake_download(CONNECTION_STRING, file_system, new_source_path+latestFolder, new_source_file)
+  new_dataframe = pd.read_excel(io.BytesIO(new_dataset), sheet_name = 'DATA_MonthEnd', header = 0, engine='openpyxl')
+
+# COMMAND ----------
+
+# validate data
+# Greate expectations https://www.architecture-performance.fr/ap_blog/built-in-expectations-in-great-expectations/
+# ----------------------------------
+val_df = new_dataframe.mask(new_dataframe == " ") # convert all blanks to NaN for validtion
+df1 = ge.from_pandas(val_df) # Create great expectations dataframe from pandas datafarme
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Tests Begin
+
+# COMMAND ----------
+
+#Test that the Date column do not contain any null values
+
+info = "Checking that the Date column do not contain any null values\n"
+expect = df1.expect_column_values_to_not_be_null(column='Date')
+test_result(expect, info)
+assert expect.success
+
+# COMMAND ----------
+
+## ServiceUserCount contains NULL value, need to find a way we can verify this
+
+# #Test that the count only contains ints
+
+# types = {
+#     "ServiceUserCount": "int"
+# }
+# info = "Checking that 'Count' column data are all int\n"
+# for column, type_ in types.items():
+#     expect = df1.expect_column_values_to_be_of_type(column=column, type_=type_)
+#     test_result(expect, info)
+#     assert expect.success
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Tests End
+
+# COMMAND ----------
+
+# Count rows in file and write to log table
+#___________________________________________
+full_path = new_source_path + latestFolder + new_source_file
+row_count = len(new_dataframe)
+today = pd.to_datetime('now').strftime("%Y-%m-%d %H:%M:%S")
+date = datetime.strptime(today, '%Y-%m-%d %H:%M:%S')
+in_row = {'row_count':[row_count], 'load_date':[date], 'file_to_load':[full_path]}
+df = pd.DataFrame(in_row)  
+write_to_sql(df, log_table, "append")
